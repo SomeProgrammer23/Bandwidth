32e030c2febd0530b2e405bc3fe4f584d0512b39
HosekiAnankos <67030981+HosekiAnankos@users.noreply.github.com>
2020-07-19 14:43:47 +1000

    Update

---
+++ Character Controller/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused;
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            Pause();
+            isPaused = !isPaused;
+        }
+       
+    }
+
+    void Pause()
+    {
+        if (isPaused)
+        {
+            pauseMenu.SetActive(isPaused);
+            Time.timeScale = 0f;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        else
+        {
+            pauseMenu.SetActive(isPaused);
+            Time.timeScale = 1f;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        
+
+    }
+}
